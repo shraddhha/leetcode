a05c2ff0d6d18a6226017d34faea92352fdedbf8
shraddhha <125660723+shraddhha@users.noreply.github.com>
2024-08-01 07:15:52 +0530

    Create 2678. Number of Senior Citizens.java

---
+++ 2678. Number of Senior Citizens.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int countSeniors(String[] details) {
+         var res = 0;
+
+        for (var d : details)
+        if (Integer.parseInt(d.substring(11, 13)) > 60)
+            res++;
+
+        return res;
+    
+        
+    }
+}
