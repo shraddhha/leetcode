b4157968c05719a4eb9f0879612dba71f71bb94b
shraddhha <125660723+shraddhha@users.noreply.github.com>
2024-08-03 08:52:51 +0530

    Create 1460. Make Two Arrays Equal by Reversing Subarrays.java

---
+++ 1460. Make Two Arrays Equal by Reversing Subarrays.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean canBeEqual(int[] target, int[] arr) {
+        var map = new int[1001];
+
+        for (var n : arr) map[n]++;
+        for (var n : target) map[n]--;
+
+        return Arrays.equals(map, new int[1001]);
+  
+        
+    }
+}
