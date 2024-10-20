e620833fee43b76bdbe1f568783148c142d1d366
shraddhha <125660723+shraddhha@users.noreply.github.com>
2024-10-20 09:09:29 +0530

    Create 1106. Parsing A Boolean Expression.java

---
+++ 1106. Parsing A Boolean Expression.java
@@ -0,0 +1,18 @@
+class Solution {
+    int idx = 0;
+    public boolean parseBoolExpr(String e) {
+        char op = e.charAt(idx++);
+        if (op == 't') return true; 
+        if (op == 'f') return false; 
+        idx++; 
+        List<Boolean> vals = new ArrayList<>();
+        while (e.charAt(idx) != ')') {
+            if (e.charAt(idx) != ',') vals.add(parseBoolExpr(e)); 
+            else idx++; 
+        }
+        idx++;
+        return op == '!' ? !vals.get(0) : op == '&' ? vals.stream().allMatch(v -> v) : vals.stream().anyMatch(v -> v);
+    
+        
+    }
+}
