b33746b7bd7d8799286a04ef5519ef2fcb0a516b
shraddhha <125660723+shraddhha@users.noreply.github.com>
2024-11-22 12:09:40 +0530

    Create 1072. Flip Columns For Maximum Number of Equal Rows.java

---
+++ 1072. Flip Columns For Maximum Number of Equal Rows.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxEqualRowsAfterFlips(int[][] matrix) {
+          Map<String, Integer> patFreq = new HashMap<>();
+        
+        for (int[] row : matrix) {
+            StringBuilder pattern = new StringBuilder();
+            if (row[0] == 0) {
+                for (int bit : row) pattern.append(bit);
+            } else {
+                for (int bit : row) pattern.append(bit ^ 1);
+            }
+            patFreq.merge(pattern.toString(), 1, Integer::sum);
+        }
+        
+        return Collections.max(patFreq.values());
+    
+
+        
+    }
+}
