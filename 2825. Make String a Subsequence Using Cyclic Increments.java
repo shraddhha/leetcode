30bce2bbeaee4f8ead0f31702ec3136e8b61da5a
shraddhha <125660723+shraddhha@users.noreply.github.com>
2024-12-04 09:20:14 +0530

    Create 2825. Make String a Subsequence Using Cyclic Increments.java

---
+++ 2825. Make String a Subsequence Using Cyclic Increments.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean canMakeSubsequence(String source, String target) {
+        int srcLen = source.length(), tgtLen = target.length();
+        char targetChar = target.charAt(0);
+        
+        int srcIdx = 0, tgtIdx = 0;
+        while (srcIdx < srcLen && tgtIdx < tgtLen) {
+            char srcChar = source.charAt(srcIdx);
+            
+            if (srcChar == targetChar || 
+                (char)(srcChar + 1) == targetChar || 
+                (srcChar == 'z' && targetChar == 'a')) {
+                tgtIdx++;
+                if (tgtIdx < tgtLen) {
+                    targetChar = target.charAt(tgtIdx);
+                }
+            }
+            
+            srcIdx++;
+        }
+        
+        return tgtIdx == tgtLen;
+    
+        
+    }
+}
